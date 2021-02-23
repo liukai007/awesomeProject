bf54330993e3bdd00cee7c4d2986352367a40fa8
liukai007 <850770722@qq.com>
2021-02-23 18:34:02 +0800

    进制转换

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module awesomeProject
 
 go 1.12
 
-require github.com/dengsgo/math-engine v0.0.0-20201031075037-706c0621418a // indirect
+require github.com/dengsgo/math-engine v0.0.0-20201031075037-706c0621418a
--- main.go
+++ main.go
@@ -2,19 +2,42 @@ package main
 
 import (
 	"fmt"
-	engine2 "src/github.com/dengsgo/math-engine/engine"
+	engine2 "github.com/dengsgo/math-engine/engine"
+	"strconv"
 )
 
-func main(){
-	s:="2+1"
+func main() {
+	//计算 公式
+	s := "2+1"
 	fmt.Println(s)
-	value,err:=engine2.ParseAndExec(s)
-	if err==nil {
+	value, err := engine2.ParseAndExec(s)
+	if err == nil {
 		fmt.Println(value)
-	}else {
+	} else {
 		fmt.Println("haha")
 	}
 
+	//得到byte的
+	a := 0x15
+	fmt.Printf("%b", a)
+	fmt.Println()
+	fmt.Printf("%d", a)
+	fmt.Println()
+	fmt.Printf("%o", a)
+	fmt.Println()
+
+	var b int64 = 11
+	fmt.Printf("%d", b)
+	fmt.Println()
+
+	s2 := strconv.FormatInt(b, 2)
+	fmt.Printf("%v", s2)
+
+	var b1 int64 = 0x15
+	fmt.Printf("%d", b1)
+	fmt.Println()
+
+	s21 := strconv.FormatInt(b1, 2)
+	fmt.Printf("%v", s21)
+
 }
-
-

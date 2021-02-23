798f7b93a20015cd4beafb88c2a102b1795c1012
liukai007 <850770722@qq.com>
2021-02-23 18:35:19 +0800

    进制转换

--- main.go
+++ main.go
@@ -40,4 +40,19 @@ func main() {
 	s21 := strconv.FormatInt(b1, 2)
 	fmt.Printf("%v", s21)
 
+	/*var v int64 = 425217101 //默认10进制
+	  s2 := strconv.FormatInt(v, 2) //10 yo 16
+	  fmt.Printf("%v\n", s2)
+
+	  s8 := strconv.FormatInt(v, 8)
+	  fmt.Printf("%v\n", s8)
+
+	  s10 := strconv.FormatInt(v, 10)
+	  fmt.Printf("%v\n", s10)
+
+	  s16 := strconv.FormatInt(v, 16) //10 yo 16
+	  fmt.Printf("%v\n", s16)
+
+	  var sv = "19584c4d"; // 16 to 10
+	  fmt.Println(strconv.ParseInt(sv, 16, 32))*/
 }
